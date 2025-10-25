6a765263859e21cc4ba1c639e0552349430aca3a
vrindagoel <vrinda.d.goel@gmail.com>
2025-10-25 14:50:27 -0400

    type: ignore

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
 import os
 
 app = Flask(__name__)
